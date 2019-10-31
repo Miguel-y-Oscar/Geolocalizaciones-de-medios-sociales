a6848acb9fe57a3ec219a52c4eece2fcd423ed19
Oscar Jimenez <osc9718@gmail.com>
2019-10-31 14:25:02 +0100

    Arreglando app #1

--- src/app.js
+++ src/app.js
@@ -4,5 +4,6 @@ var tag = require("./Entidades/tag");
 const myPost = new post("1","Twitter","MiCasa");
 const myTag = new tag("1",myPost.id,myPost.geo);
 
+#Mostrar 
 console.log(myPost.toString);
 console.log(myTag.toString);
