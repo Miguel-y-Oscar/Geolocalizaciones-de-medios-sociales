44ce9920a6306eaf9c87daf5cabb02d80b724b8d
Oscar Jimenez <osc9718@gmail.com>
2019-11-07 12:23:52 +0100

    Añadida inversión de dependencias closes #6

--- src/app.js
+++ src/app.js
@@ -1,9 +1,37 @@
-var post = require("./Entidades/post");
+var express = require('express');
+var Instagram = require('instagram-nodejs-without-api');
 var tag = require("./Entidades/tag");
+var app = express();
 
-const myPost = new post("1","Twitter","MiCasa");
-const myTag = new tag("1",myPost.id,myPost.geo);
+var instagram = new Instagram();
 
-#Mostrar 
-console.log(myPost.toString);
-console.log(myTag.toString);
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+
+app.get('/topPost/:hashtag', function(request, response, next) {
+    var hashtag = request.params.hashtag;
+    instagram.getCsrfToken().then((csrf) =>
+    {
+        instagram.csrfToken = csrf;
+    }).then(() =>
+    {
+        return instagram.searchBy('hashtag', hashtag).then(r => {
+          var textPost = r['entry_data']['TagPage'][0]['graphql']['hashtag']['edge_hashtag_to_top_posts']
+          ['edges'][0]['node']['edge_media_to_caption']['edges'][0]['node']['text'];
+          var myTag = new tag("1",textPost,"Un lugar");
+          //console.log("-->Búsqueda del top post con hastag " + hashtag);
+          //console.log("--->Post: " + textPost);
+          return response.sendStatus(200);
+        });
+        
+    }).catch(console.error);
+  });
+
+
+app.listen(3000, function () {
+    console.log('Example app listening on port 3000!');
+});
+
+module.exports = app;
--- test/post.js
+++ test/post.js
@@ -1,6 +1,8 @@
 var post = require("../src/Entidades/post");
 var tag = require("../src/Entidades/tag");
 var expect = require("chai").expect;
+var request = require('supertest');
+var	app = require('../src/app.js');
 
 describe("Test de Post", function(){
     describe("Creating post", function(){
@@ -13,7 +15,17 @@ describe("Test de Post", function(){
                     expect(myTag.toString).to.equal("1, 1, MiCasa");
                 })
             })
-
         })
     })
 })
+
+
+
+describe( "Get Top Post by HashTag", function() {
+    it('Should get top post by hashtag RakuLang', function (done) {
+    request(app)
+        .get('/topPost/RakuLang')
+        .expect('Content-Type', 'text/plain; charset=utf-8')
+        .expect(200,done);
+    });
+});
